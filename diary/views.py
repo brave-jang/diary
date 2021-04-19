65124a78cee1edf4a78ae37f328fb8c1374f5221
bravejang <wkdrmfmt@gmail.com>
2021-04-19 16:50:01 +0900

    달력 월단위 출력 성공

--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,13 @@
+import calendar
+from datetime import date
 from django.shortcuts import render
 
 
 def main(request):
-    return render(request, "diary/main.html")
+    month_days = calendar.monthcalendar(2021, 4)
+    days = [0,]
+    for week_days in month_days:
+        for week_day in week_days:
+            days.append(week_day)
+    print(date.today())
+    return render(request, "diary/main.html", {'days':days})
